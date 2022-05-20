a90e2b9214ea6dab16a78886a8d8aa4d9cea5d3c
RodrigoSD90 <rodrigo.sdnz@gmail.com>
2022-05-20 00:59:41 +0000

    commits

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)},
+  
+  {
+    path: 'death-details',
+    loadChildren: () => import('./pages/death-details/death-details.module').then( m => m.DeathDetailsPageModule)
+  },
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
